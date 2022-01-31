798320a1df57bb2b4c80a00acc229adaed6770bd
Rasulbek <rasulbekkari008@gmail.com>
2022-01-31 13:04:55 +0600

    last lone

--- src/components/Main/Content/Best/Best.jsx
+++ src/components/Main/Content/Best/Best.jsx
@@ -1,8 +1,15 @@
 import "./Best.css";
-import girl1 from "../../../../img/girl1.png";
-import asil from "../../../../img/asilbek.png";
-import aiza from "../../../../img/aiza.png";
-import jyldyz from "../../../../img/jyldyz.png";
+import activist1 from "../../../../img/активисты/image1.png";
+import activist2 from "../../../../img/активисты/image2.png";
+import activist3 from "../../../../img/активисты/image3.png";
+import activist4 from "../../../../img/активисты/image4.png";
+import activist5 from "../../../../img/активисты/image5.png";
+import activist6 from "../../../../img/активисты/image6.png";
+import activist7 from "../../../../img/активисты/image7.png";
+import activist8 from "../../../../img/активисты/image8.png";
+import activist9 from "../../../../img/активисты/image10.png";
+import activist10 from "../../../../img/активисты/image11.png";
+
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -45,65 +52,57 @@ const Best = () => {
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={girl1} alt="" />
+                    <img src={activist1} alt="" />
                   </div>
-                  <p className="title">Жылдыз</p>
+                  <p className="title">Ырысбек</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={asil} alt="" />
+                    <img src={activist2} alt="" />
                   </div>
-                  <p className="title">Асылбек</p>
+                  <p className="title">Айжан</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={aiza} alt="" />
+                    <img src={activist3} alt="" />
                   </div>
-                  <p className="title">Аиза</p>
+                  <p className="title">Асан</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={jyldyz} alt="" />
+                    <img src={activist4} alt="" />
                   </div>
-                  <p className="title">Жылдыз</p>
+                  <p className="title">Бегимай</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={girl1} alt="" />
+                    <img src={activist5} alt="" />
                   </div>
-                  <p className="title">Жылдыз</p>
+                  <p className="title">Сыймык</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={asil} alt="" />
+                    <img src={activist6} alt="" />
                   </div>
-                  <p className="title">Асылбек</p>
+                  <p className="title">Эльдар</p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="best__item">
                   <div className="best__img">
-                    <img src={aiza} alt="" />
+                    <img src={activist7} alt="" />
                   </div>
-                  <p className="title">Аиза</p>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="best__item">
-                  <div className="best__img">
-                    <img src={jyldyz} alt="" />
-                  </div>
-                  <p className="title">Жылдыз</p>
+                  <p className="title">Расулбек</p>
                 </div>
               </SwiperSlide>
             </Swiper>
